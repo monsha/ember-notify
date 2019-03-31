6f8dbe644701ddc135555fcf5016601a10544d14
Natasha Montero <natashamontero@gmail.com>
2019-03-31 15:23:01 -0400

    fix

--- addon/utils/selector-matches.js
+++ addon/utils/selector-matches.js
@@ -1,6 +1,6 @@
 export default function selectorMatches(el, selector) {
-	var p = Element.prototype;
-	var f = p.matches || p.webkitMatchesSelector || p.mozMatchesSelector || p.msMatchesSelector || function(s) {
+	const p = Element.prototype;
+	const f = p.matches || p.webkitMatchesSelector || p.mozMatchesSelector || p.msMatchesSelector || function(s) {
 		return [].indexOf.call(document.querySelectorAll(s), this) !== -1;
 	};
 	return f.call(el, selector);
